4cc674df93136c8e1d3714563d0632cabcde1c74
Jussi Pakkanen <jpakkane@gmail.com>
2017-07-14 21:58:14 +0200

    Can detect common tails.

--- dedupper.py
+++ dedupper.py
@@ -9,6 +9,27 @@ class AsmDedupper:
         self.markers = {'.cfi_startproc', '.cfi_endproc'}
 
     def dedup(self):
+        self.load_blocks()
+        for i in range(1, len(self.blocks)):
+            cur_block = self.blocks[i]
+            for j in range(i):
+                trial_block = self.blocks[j]
+                common_length = self.common_tail_length(cur_block, trial_block)
+                if common_length > 4:
+                    print('Found common block of size', common_length)
+                    print(cur_block)
+                    print(trial_block)
+                    print(i, j)
+
+    def common_tail_length(self, b1, b2):
+        i1 = len(b1)-1
+        i2 = len(b2)-1
+        while i1 >= 0 and i2 >= 0 and b1[i1] == b2[i2]: # FIXME, skip plain loop targets
+            i1 -= 1
+            i2 -= 1
+        return len(b1) - i1
+
+    def load_blocks(self):
         with(open(self.ifname)) as ifile:
             current_block = []
             for line in ifile:
@@ -19,7 +40,7 @@ class AsmDedupper:
                     current_block = []
             self.blocks.append(current_block)
 
-    def print(self):
+    def print_stats(self):
         print(self.blocks)
         print('Num of blocks:', len(self.blocks))
 
@@ -27,5 +48,5 @@ if __name__ == '__main__':
     ifile = sys.argv[1]
     ad = AsmDedupper(ifile)
     ad.dedup()
-    ad.print()
+    #ad.print_stats()
 
